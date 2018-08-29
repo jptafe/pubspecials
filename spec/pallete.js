5f1c47a749137d9e51d7a3dec0be6b5f4ed42025
EC2 Default User <ec2-user@ip-172-31-38-66.ap-southeast-1.compute.internal>
2018-08-29 07:43:57 +0000

    submit button

--- spec/pallete.js
+++ spec/pallete.js
@@ -1,6 +1,24 @@
 window.onload = function() {
     window.addEventListener("resize", unCheck);
+    document.getElementById('contentstuff').addEventListener('click', unCheck);
+    
+    var forms = document.getElementsByTagName('form');
+    for(var loop = 0;loop<forms.length;loop++) {
+        forms[loop].addEventListener('submit', function(evt) {
+            console.log(evt);
+            if(checkSubmit(this, evt.target.lastElementChild)) {
+                evt.submit(); // Going to do AJAX, but pass submit button to turn off loading...
+            } else {
+                evt.preventDefault();
+            }
+        });
+    }    
 } 
+
+function checkSubmit(form, submitButton) {
+    submitButton.value = 'Loading...';
+    return false;
+}
 
 function unCheck() {
     document.getElementById('hamburgercheck').checked = false;
